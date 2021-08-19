91b18b3a3b1e1fa14deb5821a42c1df7873171c3
NaYoung2253 <55417591+NaYoung2253@users.noreply.github.com>
2021-08-19 20:12:33 +0900

    210819 백준 11052번 카드 구매하기

---
+++ DP/11052.py
@@ -0,0 +1,13 @@
+import sys 
+
+N = int(sys.stdin.readline())
+P = [0] + list(map(int, sys.stdin.readline().split()))
+money = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        if money[i] != 0:
+            money[i] = max(money[i], P[j] + money[i - j])
+        else:
+            money[i] = P[j] + money[i - j]
+print(money[N])
